f4ae64d50aea2da475358bdc840624e88c13c624
Yied Pozi <yiedpozi@gmail.com>
2025-02-16 19:59:55 +0800

    rework on gateway class for givewp visual form builder

--- includes/class-billplz-givewp-gateway.php
+++ includes/class-billplz-givewp-gateway.php
@@ -3,6 +3,7 @@
 use Give\Donations\Models\Donation;
 use Give\Framework\PaymentGateways\Commands\GatewayCommand;
 use Give\Framework\PaymentGateways\Commands\PaymentRefunded;
+use Give\Framework\PaymentGateways\Commands\RedirectOffsite;
 use Give\Framework\PaymentGateways\PaymentGateway;
 
 if ( !defined( 'ABSPATH' ) ) exit;
@@ -32,7 +33,7 @@ class Billplz_GiveWP_Gateway extends PaymentGateway {
      */
     public function getName(): string
     {
-        return __('Billplz', 'billplz-givewp');
+        return __( 'Billplz', 'billplz-givewp' );
     }
 
     /**
@@ -40,22 +41,45 @@ class Billplz_GiveWP_Gateway extends PaymentGateway {
      */
     public function getPaymentMethodLabel(): string
     {
-        return __('Billplz', 'billplz-givewp');
+        return __( 'Billplz', 'billplz-givewp' );
+    }
+
+    // Enqueue scripts for V3 donation forms
+    public function enqueueScript( int $formId )
+    {
+        wp_enqueue_script( 'billplz-givewp', BILLPLZ_GIVEWP_URL . 'assets/js/gateway.js', array( 'react', 'wp-element' ), BILLPLZ_GIVEWP_VERSION, true );
+    }
+
+    // Send form settings to the JS gateway counterpart
+    public function formSettings(int $formId): array
+    {
+        return [
+            'message' => __( 'You will be redirected to Billplz.com to complete the donation!', 'billplz-givewp' ),
+        ];
     }
 
     /**
      * @inheritDoc
      */
-    public function createPayment(Donation $donation, $gatewayData): GatewayCommand
+    public function getLegacyFormFieldMarkup( int $formId, array $args ): string
+    {
+        return "<div class=\"billplz-givewp-help-text\">
+                    <p>" . esc_html__( 'You will be redirected to Billplz.com to complete the donation!', 'billplz-givewp' ) . "</p>
+                </div>";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function createPayment( Donation $donation, $gatewayData ): RedirectOffsite
     {
     }
 
     /**
      * @inerhitDoc
      */
-    public function refundDonation(Donation $donation): PaymentRefunded
+    public function refundDonation( Donation $donation ): PaymentRefunded
     {
+        return new PaymentRefunded();
     }
 }
-
-new Billplz_GiveWP_Gateway();
