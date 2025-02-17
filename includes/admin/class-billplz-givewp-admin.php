4e5e2c36250e71993c944d7a38870f44c7380d05
Yied Pozi <yiedpozi@gmail.com>
2025-02-17 11:41:26 +0800

    add admin class

--- includes/admin/class-billplz-givewp-admin.php
+++ includes/admin/class-billplz-givewp-admin.php
@@ -1,35 +1,37 @@
 <?php
+if ( !defined( 'ABSPATH' ) ) exit;
 
 /**
- * Give Billplz Gateway Activation
- *
- * @package     Billplz for GiveWP
- * @copyright   Copyright (c) 2019, Billplz
- * @license     https://opensource.org/licenses/gpl-license GNU Public License
- * @since       3.0.2
+ * Admin class.
+ * 
+ * @since 4.0.0
  */
+class Billplz_GiveWP_Admin {
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        add_action( 'plugin_action_links_' . BILLPLZ_GIVEWP_BASENAME, array( $this, 'register_settings_link' ) );
+    }
 
-// Exit if accessed directly
-if (!defined('ABSPATH')) {
-    exit;
+    /**
+     * Register plugin settings link.
+     * 
+     * @since 3.0.2
+     * @since 4.0.0 Moved from function.
+     * 
+     * @param array $links
+     * @return array
+     */
+    public function register_settings_link( $links ) {
+        $url = admin_url( 'edit.php?post_type=give_forms&page=give-settings&tab=gateways&section=billplz' );
+        $label = esc_html__( 'Settings', 'billplz-givewp' );
+
+        $settings_link = '<a href="' . esc_url( $url ) . '">' . $label . '</a>';
+        array_unshift( $links, $settings_link );
+
+        return $links;
+    }
 }
 
-/**
- * Plugins row action links
- *
- * @since 3.0.2
- *
- * @param array $actions An array of plugin action links.
- *
- * @return array An array of updated action links.
- */
-function give_billplz_plugin_action_links($actions)
-{
-    $new_actions = array(
-        'settings' => sprintf(
-            '<a href="%1$s">%2$s</a>', admin_url('edit.php?post_type=give_forms&page=give-settings&tab=gateways&section=billplz'), esc_html__('Settings', 'give-billplz')
-        ),
-    );
-    return array_merge($new_actions, $actions);
-}
-add_filter('plugin_action_links_' . GIVE_BILLPLZ_BASENAME, 'give_billplz_plugin_action_links');
+new Billplz_GiveWP_Admin();
