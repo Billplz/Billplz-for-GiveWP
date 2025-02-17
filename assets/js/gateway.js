8445a1f47d9fbcfb86deecd4941bdfbcf413649c
Yied Pozi <yiedpozi@gmail.com>
2025-02-17 13:47:32 +0800

    feat: render gateway fields for visual form builder

---
+++ assets/js/gateway.js
@@ -0,0 +1,34 @@
+(() => {
+    let settings = {};
+
+    // Render gateway fields
+    function BillplzGiveWPFields() {
+        return window.wp.element.createElement(
+            "div",
+            {
+                className: 'billplz-givewp-help-text'
+            },
+            window.wp.element.createElement(
+                "p",
+                {
+                    style: {marginBottom: 0}
+                },
+                settings.message,
+            )
+        );
+    }
+
+    // Gateway object
+    const BillplzGiveWPGateway = {
+        id: "billplz",
+        initialize() {
+            settings = this.settings
+        },
+        Fields() {
+            return window.wp.element.createElement(BillplzGiveWPFields);
+        },
+    };
+
+    // Register the gateway with GiveWP
+    window.givewp.gateways.register(BillplzGiveWPGateway);
+})();
